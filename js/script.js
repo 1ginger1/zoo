143dfe42e0e3870bd974b2ae8fa8fff19132f33a
Nastya <kisnastya2004@mail.ru>
2023-11-26 22:27:31 +0500

    first version page layout

---
+++ js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Табы на странице продукта
+
+    const tabs = document.querySelectorAll('.info__tab'),
+          tabContent = document.querySelectorAll('.info__content');
+
+    function hideTabContent() {
+        tabContent.forEach(content => {
+            content.classList.add('hide');
+            content.classList.remove('show');
+        });
+
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        });
+    }
+
+    hideTabContent();
+
+    function showTabContent(i = 0) {
+        tabs[i].classList.add('active');
+        
+        tabContent[i].classList.remove('hide');
+        tabContent[i].classList.add('show');
+    }
+
+    showTabContent();
+
+    tabs.forEach((tab, i) => {
+        tab.addEventListener('click', () => {
+            hideTabContent();
+            showTabContent(i);
+        });
+    });
+});
