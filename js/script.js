0db094712faf707b75b84604e55b153bdef4d2db
Nastya <kisnastya2004@mail.ru>
2023-12-01 15:58:55 +0500

    article page

--- js/script.js
+++ js/script.js
@@ -2,36 +2,37 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     // Табы на странице продукта
+    if(window.location.toString().indexOf('product.html') > 0) {
+        const tabs = document.querySelectorAll('.info__tab'),
+              tabContent = document.querySelectorAll('.info__content');
 
-    const tabs = document.querySelectorAll('.info__tab'),
-          tabContent = document.querySelectorAll('.info__content');
+        function hideTabContent() {
+            tabContent.forEach(content => {
+                content.classList.add('hide');
+                content.classList.remove('show');
+            });
 
-    function hideTabContent() {
-        tabContent.forEach(content => {
-            content.classList.add('hide');
-            content.classList.remove('show');
-        });
+            tabs.forEach(tab => {
+                tab.classList.remove('active')
+            });
+        }
 
-        tabs.forEach(tab => {
-            tab.classList.remove('active')
+        hideTabContent();
+
+        function showTabContent(i = 0) {
+            tabs[i].classList.add('active');
+            
+            tabContent[i].classList.remove('hide');
+            tabContent[i].classList.add('show');
+        }
+
+        showTabContent();
+
+        tabs.forEach((tab, i) => {
+            tab.addEventListener('click', () => {
+                hideTabContent();
+                showTabContent(i);
+            });
         });
     }
-
-    hideTabContent();
-
-    function showTabContent(i = 0) {
-        tabs[i].classList.add('active');
-        
-        tabContent[i].classList.remove('hide');
-        tabContent[i].classList.add('show');
-    }
-
-    showTabContent();
-
-    tabs.forEach((tab, i) => {
-        tab.addEventListener('click', () => {
-            hideTabContent();
-            showTabContent(i);
-        });
-    });
 });
